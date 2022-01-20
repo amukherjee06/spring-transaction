ffe4e6d62e705eda0df73143eaf6a9ce91d254e3
Arnab Mukherjee <mukherjeearnab06@gmail.com>
2022-01-20 17:45:08 +0530

    01.Initial checkin

---
+++ spring-transactional/src/main/java/com/arnab/spring/transaction/spring/transaction/SpringTransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.transaction.spring.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionalApplication.class, args);
+	}
+
+}
---
+++ spring-transactional/src/test/java/com/arnab/spring/transaction/spring/transaction/SpringTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.transaction.spring.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
